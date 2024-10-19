77b8e254d33340b2790447eb2f724f80949a070d
Anne Medina <carmelaanne.medina@gmail.com>
2024-10-19 12:00:39 +1030

    My travel journal solo project

---
+++ components/Card.js
@@ -0,0 +1,10 @@
+import React from "react"
+import data from "../data.js"
+
+export default function Cards(){
+    return (
+        <main>
+        
+        </main>
+    )
+}
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <img className="navbar--logo" src="../assets/Globe.png"/> my travel journal.
+        </nav>
+    )
+}

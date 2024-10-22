9d9cd00410257018cd6d58ef0ea2bb807b84d7e9
Anne Medina <carmelaanne.medina@gmail.com>
2024-10-22 19:50:22 +1030

    Corrected some details in data to pinpoint exact location in maps

--- data.js
+++ data.js
@@ -11,18 +11,18 @@ export default [
     {
         title: "Sydney Opera House",
         location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
+        googleMapsUrl: "https://maps.app.goo.gl/dDrHnY4ub1XNjufz6",
+        startDate: "27 May, 2023",
+        endDate: "8 Jun, 2023",
         description: "The Sydney Opera House is a multi-venue performing arts center in Sydney. Located on the banks of Sydney Harbour, it is often regarded as one of the 20th century's most famouse and distinctive buildings.",
         imageUrl: "https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?cs=tinysrgb&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUyNjF8&ixlib=rb-4.0.3&w=125&h=168&fit=crop&crop=entropy,right"
     },
     {
         title: "Geirangerfjord",
         location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "21 Oct, 2021",
-        endDate: "18 Nov, 2021",
+        googleMapsUrl: "https://maps.app.goo.gl/8ysVeovyuMx4g6kW8",
+        startDate: "01 Oct, 2024",
+        endDate: "18 Nov, 2024",
         description: "The Geiranger Fjord is a fjord in the Sunnmore region of More og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: "https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?cs=tinysrgb&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUzMTF8&ixlib=rb-4.0.3&q=80&w=125&h=168&fit=crop&crop=left,right,entropy"
     }
